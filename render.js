19a7fb08cd759b2d863c6297f08db3244217200e
David Hoksza <david.hoksza@gmail.com>
2023-04-18 19:12:50 +0200

    vis generator

--- render.js
+++ render.js
@@ -1,27 +1,76 @@
-function renderData(svg, data, params, colClass='group'){
+function getExtents(data){
+    extents = {}
+    const dimensions = Object.keys(data[0]);
+    for (const name of dimensions) {    
+        extents[name] = d3.extent(data, function(d) { return parseFloat(d[name])});
+    }
+    return extents;
+}
+
+function filterDataByCols(data, colsRemove) {
+    if (!colsRemove.length) return data;
+    return data.map(d => {                    
+        for (const col of colsRemove){
+            delete d[col]
+        }
+        return d;
+    })
+}
+
+function filterDataByClassSize(data, size=500) {
+    let clsCounts = {}
+    return data.filter(d => {
+        const cls = d[colClass]
+        if (clsCounts[cls] === undefined) {
+            clsCounts[cls] = 0;
+        }
+        clsCounts[cls]++;
+        return clsCounts[cls] <= size;
+
+    })
+}
+
+function getWidth(params){
+    return params.dims.axisWidth == undefined ? params.dims.width - params.margin.left - params.margin.right : params.dims.axisWidth * Object.keys(data[0]).length;
+}
+
+
+function renderData(svg, data, params, extents=null){
+
+    colClass = params ? params.dataSource.categoryColumn : 'group';
     
     // let classes = Array.from(new Set(data.map(x=>x[colClass])));
     // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
     //dimensions = Object.keys(data[0]).filter(function(d) { return d != "Species" })
-    let dimensions = Object.keys(data[0]).slice(2);
+    //let dimensions = Object.keys(data[0]).slice(2);
+    let dimensions = Object.keys(data[0]);
+    var ix = dimensions.indexOf(colClass);
+    if (ix !== -1) {
+        dimensions.splice(ix, 1);
+    }
     
     // console.log(dimensions);
     // console.log(classes);        
     // var colorScale = d3.scalePoint().domain(classes)
     // var color = classes.length > 10 ? d => d3.interpolateSpectral(colorScale(d[colClass])) : d => d3.scaleOrdinal().domain(classes).range(d3.schemeTableau10)(d);
 
-    //console.log(classes[0], color(classes[0]))
-    // For each dimension, I build a linear scale. I store all in a y object
-    var y = {}        
-    for (const name of dimensions) {            
-        y[name] = d3.scaleLinear()                
-            .domain( d3.extent(data, function(d) { return parseFloat(d[name]); }) )
-            .range([params.dims.height, 0])
+    if (extents === null) {
+        extents = getExtents(data);
     }
 
+    //console.log(classes[0], color(classes[0]))
+    // For each dimension, I build a linear scale. I store all in a y object
+    var y = {}            
+    for (const name of dimensions) {            
+        y[name] = d3.scaleLinear()                
+            .domain( extents[name])
+            .range([params.dims.height-params.margin.top-params.margin.bottom, 0])
+    }
+
+    
     // Build the X scale -> it find the best position for each Y axis
     x = d3.scalePoint()
-        .range([0, params.dims.width])
+        .range([0, getWidth(params)])
         .padding(1)
         .domain(dimensions);
 
@@ -55,7 +104,8 @@ function renderData(svg, data, params, colClass='group'){
             // Add axis title
             .append("text")
             .style("text-anchor", "middle")
-            .attr("y", -9)
+            .style("transform", "rotate(-45deg)")
+            //.attr("y", -9)
             .text(function(d) { return d; })
             .style("fill", "black")
 
@@ -64,7 +114,7 @@ function renderData(svg, data, params, colClass='group'){
 
 }
 
-function saveSvg(svgEl, name) {
+async function saveSvg(svgEl, name) {
     svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
     var svgData = svgEl.outerHTML;
     var preface = '<?xml version="1.0" standalone="no"?>\r\n';
@@ -78,5 +128,13 @@ function saveSvg(svgEl, name) {
     downloadLink.download = name;
     document.body.appendChild(downloadLink);
     downloadLink.click();
-    //document.body.removeChild(downloadLink);    
+    document.body.removeChild(downloadLink);      
+}
+
+function emptySelection(node) {
+    node.selectAll("*").remove();
+}
+
+function sanitaizeName(name){
+    return name.replace(' ', '_');
 }
--- run-selenium.py
+++ run-selenium.py
@@ -11,14 +11,22 @@ options = webdriver.ChromeOptions()
 prefs = {}
 prefs["profile.default_content_settings.popups"]=0
 prefs["download.default_directory"]= os.path.dirname(os.path.realpath(__file__))
+prefs["profile.content_settings.exceptions.automatic_downloads.*.setting"] = 1;
 options.add_experimental_option("prefs", prefs)
 # options.add_argument("start-maximized")
-# options.add_experimental_option("detach", True)
+options.add_experimental_option("detach", True)
 
 # service = Service(executable_path="c:/WebDriver/bin/chromedriver")
 # driver = webdriver.Chrome(service=service, options=options)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("http://localhost:8080/single-vis.html")
+# driver.get("http://localhost:8080/single-vis.html")
 
-el = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID,"download"))
-driver.implicitly_wait(3);
+#driver.get("http://localhost:8080/class-vis-generator.html")
+driver.get("http://localhost:8080/class-vis-generator.html?path=data/nutrients.csv&name=nutrients&categoryColumn=group&filterOutColumns=name")
+# driver.get("http://localhost:8080/class-vis-generator.html?path=data/breastCancer.csv&name=breastCancer&categoryColumn=diagnosis&filterOutColumns=")
+# driver.get("http://localhost:8080/class-vis-generator.html?path=data/recipes.csv&name=recipes&categoryColumn=category&filterOutColumns=")
+#driver.get("http://localhost:8080/class-vis-generator.html?path=data/spotify.csv&name=spotify&categoryColumn=genre&filterOutColumns=track_id")
+
+
+# el = WebDriverWait(driver, 10).until(lambda d: d.find_element(By.ID,"download"))
+# driver.implicitly_wait(3);
